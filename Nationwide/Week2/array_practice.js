79fffd062964494f25c25606f96c22f46d9f84a4
AbeTavarez <abeincwireless@gmail.com>
2021-07-22 20:12:30 -0400

    arrays

---
+++ Nationwide/Week2/array_practice.js
@@ -0,0 +1,25 @@
+let firstArray = ['red', 'green', 'blue']
+console.log(firstArray)
+
+//Prints the data type of the object
+console.log(typeof firstArray)
+
+//Checks if object is an array (returns a boolean)
+console.log(Array.isArray(firstArray))
+
+//Creates a new array
+let faveFoods = ['pizza', 'hotdog', 'tacos']
+console.log(faveFoods)
+
+// Print first element in the array
+console.log(faveFoods[0])
+console.log(faveFoods[2])
+
+//Prints the length of the array
+console.log(faveFoods.length)
+
+// -----------------------------------
+const numbers = [21,18,5,3,2,1,1]
+console.log(numbers)
+numbers[2] = null
+console.log(numbers)
