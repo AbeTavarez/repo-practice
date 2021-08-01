e251a235f1354c4af7501d8d8169c7036da3e0cb
AbeTavarez <abeincwireless@gmail.com>
2021-07-31 22:04:44 -0400

    w3d3

--- Nationwide/Week3/day2/jquery/jquery-examples/app.js
+++ Nationwide/Week3/day2/jquery/jquery-examples/app.js
@@ -4,26 +4,28 @@ $(() => {
 // prints jquery document
 console.log($)
 
-// creates new div and appends a class to the div
+// creates new div
 const $div = $('<div>');
+//appends a class to the div
 $div.addClass('meat')
 
 
-console.log($div)
+console.log($div);
 
+// grabs the body tag and logs it
+console.log($('body'));
 // appends new $div to the body
-console.log($('body'))
-$('body').append($div)
+$('body').append($div);
 
 // create h3 and appends it to the $div
 const $h3 = $('<h3>').text('Ostrich');
-$div.append($h3)
+$div.append($h3);
 
-//* your turn
+//* Your turn
 const myDiv = $('<div>'); 
 const myH3 = $('<h3>')
-myDiv.append(myH3).text('My Div').addClass('dairy')
-$('body').append(myDiv)
+myDiv.append(myH3).text('My Div').addClass('dairy');
+$('body').append(myDiv);
 
 const $divs = $('div');
 console.log($divs)
---
+++ Nationwide/Week3/day3/answers.js
@@ -0,0 +1,20 @@
+
+
+//* Take it Easy
+
+// 1
+const raibowColors = ['violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red'];
+// 2
+const blue = raibowColors[2];
+console.log(`Exe.2 ${blue}`)
+// 3
+const abe = {};
+abe.favFood = 'pizza';
+abe.hobby = 'swimming';
+abe.town = 'nyc';
+abe.favdatatype = 'objects';
+
+console.log('Exe. 3', abe)
+// 4
+console.log('Exe. 4', abe.hobby)
+
