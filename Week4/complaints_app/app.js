a7f17ec08715670fbdade336b42ad952556af7f0
abeTavarez <abeincwireless@gmail.com>
2021-08-09 00:41:42 -0400

    lab missing toggle

--- Week4/complaints_app/app.js
+++ Week4/complaints_app/app.js
@@ -1,78 +1,113 @@
 console.log(`running script...`)
+$('#result-h2').hide()
 
-//* 1- Handle user input data (number of complaints to search)
 //* ================== Form data
-let numOfComplaints;
-// Attach event listener to the form on the html page
+//* 1- Attach event listener to the form on the html page
 $('form').on('submit', e => {
+    // console.log(e.originalEvent.submitter.id)
     e.preventDefault(); // prevent page from refreshing on submit
-    // get user input for number of complaints
-    numOfComplaints = $('input[type="text"]').val();
-})
 
-//* 2- Handle the user button interaction (identify which button the user pressed)
-//* =================== Buttons
-// gets all the buttons from the html page
-const buttons = $(':button')
-// console.log(buttons)
+    //* 2- Get user input for number of complaints
+    const numOfComplaints = $('input[type="text"]').val();
+    let btnBorough; // variable to set which borough button was pressed
 
-//* Check which button was clicked
-// 1. Attach event listener to the buttons
-buttons.click((e) => {
-    // 2. declare a variable to set which button was pressed
-    let btnPressed; 
+    //* =================== Buttons
+    //* 3- Check which button was clicked
+    // 1. Attach event listener to the button
+    const buttonId = e.originalEvent.submitter.id;  
     // 3. check button id and sets btnPressed to that id
-    if (e.target.id === 'manhattan'){
-        btnPressed = 'Manhattan';
-        console.log(btnPressed)
+    if (buttonId === 'manhattan'){
+        btnBorough = 'MANHATTAN';
     } 
-    else if (e.target.id === 'brooklyn'){
-        btnPressed = 'Brooklyn';
+    else if (buttonId === 'brooklyn'){
+        btnBorough = 'BROOKLYN';
     }
-    else if (e.target.id === 'queens'){
-        btnPressed = 'Queens';
+    else if (buttonId === 'queens'){
+        btnBorough = 'QUEENS';
     }
-    else if (e.target.id === 'staten_island'){
-        btnPressed = 'Staten Island';
+    else if (buttonId === 'staten_island'){
+        btnBorough = 'STATEN ISLAND';
     }
-    else if (e.target.id === 'bronx'){
-        btnPressed = 'Bronx';
-        console.log(btnPressed)
+    else if (buttonId === 'bronx'){
+        btnBorough = 'BRONX';
     };
 
-    //* Make API call
-    // checks if numOfComplaints has a value 
+    //* ==================================== Make API call
+    //* ===== checks if numOfComplaints has a value 
     if (numOfComplaints){
+        $('#result-h2').show()
         // create ajax call with numOfComplains
-        
-        } else {
-        // make default ajax call with limit 10 from NYPD
-
         const promise = $.ajax({
-        url: `https://data.cityofnewyork.us/resource/erm2-nwe9.json?$limit=10&agency=NYPD`
+            url: 'https://data.cityofnewyork.us/resource/erm2-nwe9.json?$limit=' + numOfComplaints + '&agency=NYPD' + '&borough=' + btnBorough
+            });   
+        // Show data to the user    
+        promise.then(
+            data => {
+                data.map( complaint => {
+                    console.log(complaint)
+                    let agency = $('<div>').html(complaint.agency)
+                    $('#complaints-container').append(agency)
+
+                    let borough = $('<div>').html(complaint.borough)
+                    $('#complaints-container').append(borough)
+
+                    let complaintTag = $('<div>').html(complaint.complaint)
+                    $('#complaints-container').append(complaintTag)
+
+                    let description = $('<div>').html(complaint.descriptor)
+                    $('#complaints-container').append(description)
+
+                    let status = $('<div>').html(complaint.status)
+                    $('#complaints-container').append(status)
+                })
+            },
+            error => console.log(error)
+        );
+        //* ====== if no number os complaints was enter
+        } else {
+            $('#result-h2').show()
+        // make default ajax call with limit 10 from NYPD
+        const promise = $.ajax({
+        url: `https://data.cityofnewyork.us/resource/erm2-nwe9.json?$limit=10&agency=NYPD` + '&borough=' + btnBorough
         });   
 
         promise.then(
-            data => console.log(data),
+            data => {
+                data.map( complaint => {
+                    console.log(complaint)
+                    let agency = $('<div>').html(complaint.agency)
+                    $('#complaints-container').append(agency)
+
+                    let borough = $('<div>').html(complaint.borough)
+                    $('#complaints-container').append(borough)
+
+                    let complaintTag = $('<div>').html(complaint.complaint)
+                    $('#complaints-container').append(complaintTag)
+
+                    let description = $('<div>').html(complaint.descriptor)
+                    $('#complaints-container').append(description)
+
+                    let status = $('<div>').html(complaint.status)
+                    $('#complaints-container').append(status)
+                    
+                   
+                
+                })
+            },
             error => console.log(error)
         )
         
-        }
+        };
+        e.stopPropagation();
+});
 
-})
+// $('#borough').html(data[index].borough)
+// $('#complaint').html(data[index].complaint)
+// $('#description').html(data[index].descriptor)
+// $('#status').html(data[index].status)
 
 
 
-
-
-
-// const promise = $.ajax({
-//     url: "https://data.cityofnewyork.us/resource/erm2-nwe9.json"
-// });
-
-// promise.then(
-//     data => console.log(data[0].agency_name)
-// )
 
 // promise.then(
 //     // on success
