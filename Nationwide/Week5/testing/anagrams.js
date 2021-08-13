55f9589da51a14f4b8383eaef3180f5ec9dd9eb2
abeTavarez <abeincwireless@gmail.com>
2021-08-13 00:22:09 -0400

    jest lesson

--- Nationwide/Week5/testing/anagrams.js
+++ Nationwide/Week5/testing/anagrams.js
@@ -1,8 +1,24 @@
 
-const cleanStr = (str) => {
+// * =========== Helper function to clean the string
+
+const cleanStr = str => {
     return str.replace(/[^\w]/g,'') // remove white spaces
     .toLowerCase() // turn string into all lower case
     .split('') // turn string into array
     .sort() // sort the array
     .join() // turn array back to string
-}
+};
+
+//* ========= Anagram function
+const anagrams = (strA, strB) => {
+    strA = cleanStr(strA)
+    // console.log(strA)
+    strB =  cleanStr(strB)
+    // console.log(strB)
+    return  strA === strB
+};
+console.log(anagrams(`Hello World`, `! dlrowllohe`))
+console.log(anagrams(`Hello World`, `helloo world`))
+
+//* Exporting the function will make it available in others files on out project
+module.exports = anagrams;
